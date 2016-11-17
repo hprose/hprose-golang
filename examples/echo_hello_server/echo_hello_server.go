e1030e40d6b282536649d04861992877e4c387f2
Ma Bingyao <mabingyao@gmail.com>
2016-11-17 21:10:44 +0800

    Update echo_hello_server.go

--- examples/echo_hello_server/echo_hello_server.go
+++ examples/echo_hello_server/echo_hello_server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/hprose/hprose-golang/rpc"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 )
 
 func hello(name string) string {
@@ -14,6 +13,6 @@ func main() {
 	service := rpc.NewHTTPService()
 	service.AddFunction("hello", hello)
 	e := echo.New()
-	e.Any("/hello", standard.WrapHandler(service))
+	e.Any("/hello", echo.WrapHandler(service))
 	e.Run(standard.New(":8080"))
 }
