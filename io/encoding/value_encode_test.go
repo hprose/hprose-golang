a83a1469c324b944cb7e430803235cee684de225
andot <mabingyao@gmail.com>
2020-02-22 23:24:12 +0800

    Create value_encode_test.go

---
+++ io/encoding/value_encode_test.go
@@ -0,0 +1,262 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| io/encoding/value_encode_test.go                         |
+|                                                          |
+| LastModified: Feb 22, 2020                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package encoding
+
+import (
+	"math"
+	"math/big"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestWriteInt(t *testing.T) {
+	sb := new(strings.Builder)
+	if err := WriteInt(sb, -1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, 0); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, 1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, 123); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, math.MinInt64); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, -math.MaxInt64); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt(sb, math.MaxInt64); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "i-1;01i123;l-9223372036854775808;l-9223372036854775807;l9223372036854775807;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteUint(t *testing.T) {
+	sb := new(strings.Builder)
+	if err := WriteUint(sb, 0); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint(sb, 1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint(sb, 123); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint(sb, math.MaxUint64); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint(sb, math.MaxUint32); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint(sb, math.MaxInt32); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "01i123;l18446744073709551615;l4294967295;i2147483647;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteInt32(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteInt32(sb, -1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt32(sb, 0); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt32(sb, 1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt32(sb, 123); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt32(sb, math.MinInt32); err != nil {
+		t.Error(err)
+	}
+	if err := WriteInt32(sb, math.MaxInt32); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "i-1;01i123;i-2147483648;i2147483647;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteUint32(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteUint32(sb, 0); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint32(sb, 1); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint32(sb, 123); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint32(sb, math.MaxUint32); err != nil {
+		t.Error(err)
+	}
+	if err := WriteUint32(sb, math.MaxInt32); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "01i123;l4294967295;i2147483647;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteNil(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteNil(sb); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "n" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteBool(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteBool(sb, true); err != nil {
+		t.Error(err)
+	}
+	if err := WriteBool(sb, false); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "tf" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteFloat(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteFloat32(sb, math.E); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat32(sb, math.Pi); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, math.E); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, math.Pi); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, math.Log(1)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, math.Log(0)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, -math.Log(0)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteFloat64(sb, math.Log(-1)); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "d2.7182817;d3.1415927;d2.718281828459045;d3.141592653589793;d0;I-I+N" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteString(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteString(sb, ""); err != nil {
+		t.Error(err)
+	}
+	if err := WriteString(sb, "Hello"); err != nil {
+		t.Error(err)
+	}
+	if err := WriteString(sb, "中文"); err != nil {
+		t.Error(err)
+	}
+	if err := WriteString(sb, "🐱🐶"); err != nil {
+		t.Error(err)
+	}
+	if err := WriteString(sb, "👩‍👩‍👧‍👧"); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != `s""s5"Hello"s2"中文"s4"🐱🐶"s11"👩‍👩‍👧‍👧"` {
+		t.Error(sb)
+	}
+}
+
+func TestWriteBytes(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteBytes(sb, []byte("")); err != nil {
+		t.Error(err)
+	}
+	if err := WriteBytes(sb, []byte("Hello")); err != nil {
+		t.Error(err)
+	}
+	if err := WriteBytes(sb, []byte("中文")); err != nil {
+		t.Error(err)
+	}
+	if err := WriteBytes(sb, []byte("🐱🐶")); err != nil {
+		t.Error(err)
+	}
+	if err := WriteBytes(sb, []byte("👩‍👩‍👧‍👧")); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != `b""b5"Hello"b6"中文"b8"🐱🐶"b25"👩‍👩‍👧‍👧"` {
+		t.Error(sb)
+	}
+}
+
+func TestWriteBigInt(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteBigInt(sb, big.NewInt(math.MaxInt64)); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "l9223372036854775807;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteBigFloat(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteBigFloat(sb, big.NewFloat(math.MaxFloat64)); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "d1.7976931348623157e+308;" {
+		t.Error(sb)
+	}
+}
+
+func TestWriteTime(t *testing.T) {
+	sb := &strings.Builder{}
+	if err := WriteTime(sb, time.Date(2020, 2, 22, 0, 0, 0, 0, time.UTC)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 0, time.UTC)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 123456789, time.Local)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123456000, time.Local)); err != nil {
+		t.Error(err)
+	}
+	if err := WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123000000, time.UTC)); err != nil {
+		t.Error(err)
+	}
+	if sb.String() != "D20200222ZT121212ZT121212.123456789;D20200222T121212.123456;D20200222T121212.123Z" {
+		t.Error(sb)
+	}
+}
