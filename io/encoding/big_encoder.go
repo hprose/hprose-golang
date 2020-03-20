be752534a77407bb52e9368a6ec7b946171b2d68
andot <mabingyao@gmail.com>
2020-03-21 00:20:33 +0800

    Update big_encoder.go

--- io/encoding/big_encoder.go
+++ io/encoding/big_encoder.go
@@ -6,7 +6,7 @@
 |                                                          |
 | io/encoding/big_encoder.go                               |
 |                                                          |
-| LastModified: Mar 19, 2020                               |
+| LastModified: Mar 20, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -15,7 +15,8 @@ package encoding
 
 import (
 	"math/big"
-	"reflect"
+
+	"github.com/modern-go/reflect2"
 )
 
 // BigIntEncoder is the implementation of ValueEncoder for big.Int/*bit.Int.
@@ -30,13 +31,7 @@ func (valenc BigIntEncoder) Encode(enc *Encoder, v interface{}) (err error) {
 // Write writes the hprose encoding of v to stream
 // if v is already written to stream, it will writes it as value
 func (BigIntEncoder) Write(enc *Encoder, v interface{}) (err error) {
-	switch v := v.(type) {
-	case big.Int:
-		return WriteBigInt(enc.Writer, &v)
-	case *big.Int:
-		return WriteBigInt(enc.Writer, v)
-	}
-	return &UnsupportedTypeError{Type: reflect.TypeOf(v)}
+	return WriteBigInt(enc.Writer, (*big.Int)(reflect2.PtrOf(v)))
 }
 
 // BigFloatEncoder is the implementation of ValueEncoder for big.Float/*bit.Float.
@@ -51,13 +46,7 @@ func (valenc BigFloatEncoder) Encode(enc *Encoder, v interface{}) (err error) {
 // Write writes the hprose encoding of v to stream
 // if v is already written to stream, it will writes it as value
 func (BigFloatEncoder) Write(enc *Encoder, v interface{}) (err error) {
-	switch v := v.(type) {
-	case big.Float:
-		return WriteBigFloat(enc.Writer, &v)
-	case *big.Float:
-		return WriteBigFloat(enc.Writer, v)
-	}
-	return &UnsupportedTypeError{Type: reflect.TypeOf(v)}
+	return WriteBigFloat(enc.Writer, (*big.Float)(reflect2.PtrOf(v)))
 }
 
 // BigRatEncoder is the implementation of ValueEncoder for big.Rat/*bit.Rat.
@@ -72,13 +61,7 @@ func (valenc BigRatEncoder) Encode(enc *Encoder, v interface{}) (err error) {
 // Write writes the hprose encoding of v to stream
 // if v is already written to stream, it will writes it as value
 func (BigRatEncoder) Write(enc *Encoder, v interface{}) (err error) {
-	switch v := v.(type) {
-	case big.Rat:
-		return WriteBigRat(enc, &v)
-	case *big.Rat:
-		return WriteBigRat(enc, v)
-	}
-	return &UnsupportedTypeError{Type: reflect.TypeOf(v)}
+	return WriteBigRat(enc, (*big.Rat)(reflect2.PtrOf(v)))
 }
 
 func init() {
