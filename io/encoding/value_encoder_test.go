3bda318edf6bd42428a89889a8a8f8af6c0a4c88
andot <mabingyao@gmail.com>
2020-03-21 00:21:24 +0800

    Update value_encoder_test.go

--- io/encoding/value_encoder_test.go
+++ io/encoding/value_encoder_test.go
@@ -6,7 +6,7 @@
 |                                                          |
 | io/encoding/value_encode_test.go                         |
 |                                                          |
-| LastModified: Mar 15, 2020                               |
+| LastModified: Mar 20, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -19,278 +19,132 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWriteInt(t *testing.T) {
 	sb := new(strings.Builder)
-	if err := WriteInt(sb, -1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, math.MinInt64); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, -math.MaxInt64); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt(sb, math.MaxInt64); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "i-1;01i123;l-9223372036854775808;l-9223372036854775807;l9223372036854775807;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteInt(sb, -1))
+	assert.NoError(t, WriteInt(sb, 0))
+	assert.NoError(t, WriteInt(sb, 1))
+	assert.NoError(t, WriteInt(sb, 123))
+	assert.NoError(t, WriteInt(sb, math.MinInt64))
+	assert.NoError(t, WriteInt(sb, -math.MaxInt64))
+	assert.NoError(t, WriteInt(sb, math.MaxInt64))
+	assert.Equal(t, "i-1;01i123;l-9223372036854775808;l-9223372036854775807;l9223372036854775807;", sb.String())
 }
 
 func TestWriteUint(t *testing.T) {
 	sb := new(strings.Builder)
-	if err := WriteUint(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint(sb, math.MaxUint64); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint(sb, math.MaxUint32); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint(sb, math.MaxInt32); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;l18446744073709551615;l4294967295;i2147483647;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteUint(sb, 0))
+	assert.NoError(t, WriteUint(sb, 1))
+	assert.NoError(t, WriteUint(sb, 123))
+	assert.NoError(t, WriteUint(sb, math.MaxUint64))
+	assert.NoError(t, WriteUint(sb, math.MaxUint32))
+	assert.NoError(t, WriteUint(sb, math.MaxInt32))
+	assert.Equal(t, "01i123;l18446744073709551615;l4294967295;i2147483647;", sb.String())
 }
 
 func TestWriteInt32(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteInt32(sb, -1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt32(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt32(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt32(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt32(sb, math.MinInt32); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt32(sb, math.MaxInt32); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "i-1;01i123;i-2147483648;i2147483647;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteInt32(sb, -1))
+	assert.NoError(t, WriteInt32(sb, 0))
+	assert.NoError(t, WriteInt32(sb, 1))
+	assert.NoError(t, WriteInt32(sb, 123))
+	assert.NoError(t, WriteInt32(sb, math.MinInt32))
+	assert.NoError(t, WriteInt32(sb, math.MaxInt32))
+	assert.Equal(t, "i-1;01i123;i-2147483648;i2147483647;", sb.String())
 }
 
 func TestWriteUint32(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteUint32(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint32(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint32(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint32(sb, math.MaxUint32); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint32(sb, math.MaxInt32); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;l4294967295;i2147483647;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteUint32(sb, 0))
+	assert.NoError(t, WriteUint32(sb, 1))
+	assert.NoError(t, WriteUint32(sb, 123))
+	assert.NoError(t, WriteUint32(sb, math.MaxUint32))
+	assert.NoError(t, WriteUint32(sb, math.MaxInt32))
+	assert.Equal(t, "01i123;l4294967295;i2147483647;", sb.String())
 }
 
 func TestWriteUint16(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteUint16(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint16(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint16(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint16(sb, math.MaxUint16); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint16(sb, math.MaxInt16); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;i65535;i32767;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteUint16(sb, 0))
+	assert.NoError(t, WriteUint16(sb, 1))
+	assert.NoError(t, WriteUint16(sb, 123))
+	assert.NoError(t, WriteUint16(sb, math.MaxUint16))
+	assert.NoError(t, WriteUint16(sb, math.MaxInt16))
+	assert.Equal(t, "01i123;i65535;i32767;", sb.String())
 }
 
 func TestWriteInt16(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteInt16(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt16(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt16(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt16(sb, math.MinInt16); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt16(sb, math.MaxInt16); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;i-32768;i32767;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteInt16(sb, 0))
+	assert.NoError(t, WriteInt16(sb, 1))
+	assert.NoError(t, WriteInt16(sb, 123))
+	assert.NoError(t, WriteInt16(sb, math.MinInt16))
+	assert.NoError(t, WriteInt16(sb, math.MaxInt16))
+	assert.Equal(t, "01i123;i-32768;i32767;", sb.String())
 }
 
 func TestWriteUint8(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteUint8(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint8(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint8(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint8(sb, math.MaxUint8); err != nil {
-		t.Error(err)
-	}
-	if err := WriteUint8(sb, math.MaxInt8); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;i255;i127;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteUint8(sb, 0))
+	assert.NoError(t, WriteUint8(sb, 1))
+	assert.NoError(t, WriteUint8(sb, 123))
+	assert.NoError(t, WriteUint8(sb, math.MaxUint8))
+	assert.NoError(t, WriteUint8(sb, math.MaxInt8))
+	assert.Equal(t, "01i123;i255;i127;", sb.String())
 }
 
 func TestWriteInt8(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteInt8(sb, 0); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt8(sb, 1); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt8(sb, 123); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt8(sb, math.MinInt8); err != nil {
-		t.Error(err)
-	}
-	if err := WriteInt8(sb, math.MaxInt8); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "01i123;i-128;i127;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteInt8(sb, 0))
+	assert.NoError(t, WriteInt8(sb, 1))
+	assert.NoError(t, WriteInt8(sb, 123))
+	assert.NoError(t, WriteInt8(sb, math.MinInt8))
+	assert.NoError(t, WriteInt8(sb, math.MaxInt8))
+	assert.Equal(t, "01i123;i-128;i127;", sb.String())
 }
 
 func TestWriteBool(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteBool(sb, true); err != nil {
-		t.Error(err)
-	}
-	if err := WriteBool(sb, false); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "tf" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteBool(sb, true))
+	assert.NoError(t, WriteBool(sb, false))
+	assert.Equal(t, "tf", sb.String())
 }
 
 func TestWriteFloat(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteFloat32(sb, math.E); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat32(sb, math.Pi); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, math.E); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, math.Pi); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, math.Log(1)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, math.Log(0)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, -math.Log(0)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteFloat64(sb, math.Log(-1)); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "d2.7182817;d3.1415927;d2.718281828459045;d3.141592653589793;d0;I-I+N" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteFloat32(sb, math.E))
+	assert.NoError(t, WriteFloat32(sb, math.Pi))
+	assert.NoError(t, WriteFloat64(sb, math.E))
+	assert.NoError(t, WriteFloat64(sb, math.Pi))
+	assert.NoError(t, WriteFloat64(sb, math.Log(1)))
+	assert.NoError(t, WriteFloat64(sb, math.Log(0)))
+	assert.NoError(t, WriteFloat64(sb, -math.Log(0)))
+	assert.NoError(t, WriteFloat64(sb, math.Log(-1)))
+	assert.Equal(t, "d2.7182817;d3.1415927;d2.718281828459045;d3.141592653589793;d0;I-I+N", sb.String())
 }
 
 func TestWriteBigInt(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteBigInt(sb, big.NewInt(math.MaxInt64)); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "l9223372036854775807;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteBigInt(sb, big.NewInt(math.MaxInt64)))
+	assert.Equal(t, "l9223372036854775807;", sb.String())
 }
 
 func TestWriteBigFloat(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteBigFloat(sb, big.NewFloat(math.MaxFloat64)); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "d1.7976931348623157e+308;" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteBigFloat(sb, big.NewFloat(math.MaxFloat64)))
+	assert.Equal(t, "d1.7976931348623157e+308;", sb.String())
 }
 
 func TestWriteTime(t *testing.T) {
 	sb := &strings.Builder{}
-	if err := WriteTime(sb, time.Date(2020, 2, 22, 0, 0, 0, 0, time.UTC)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 0, time.UTC)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 123456789, time.Local)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123456000, time.Local)); err != nil {
-		t.Error(err)
-	}
-	if err := WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123000000, time.UTC)); err != nil {
-		t.Error(err)
-	}
-	if sb.String() != "D20200222ZT121212ZT121212.123456789;D20200222T121212.123456;D20200222T121212.123Z" {
-		t.Error(sb)
-	}
+	assert.NoError(t, WriteTime(sb, time.Date(2020, 2, 22, 0, 0, 0, 0, time.UTC)))
+	assert.NoError(t, WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 0, time.UTC)))
+	assert.NoError(t, WriteTime(sb, time.Date(1970, 1, 1, 12, 12, 12, 123456789, time.Local)))
+	assert.NoError(t, WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123456000, time.Local)))
+	assert.NoError(t, WriteTime(sb, time.Date(2020, 2, 22, 12, 12, 12, 123000000, time.UTC)))
+	assert.Equal(t, "D20200222ZT121212ZT121212.123456789;D20200222T121212.123456;D20200222T121212.123Z", sb.String())
 }
