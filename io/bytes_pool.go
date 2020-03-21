b5ad4d03d52d1d2f51de0b01af742cc661d17cab
andot <mabingyao@gmail.com>
2020-03-21 09:48:53 +0800

    rename byte_pool to bytes_pool

--- io/bytes_pool.go
+++ io/bytes_pool.go
@@ -4,9 +4,9 @@
 |                                                          |
 | Official WebSite: https://hprose.com                     |
 |                                                          |
-| io/byte_pool.go                                          |
+| io/bytes_pool.go                                         |
 |                                                          |
-| LastModified: Feb 22, 2020                               |
+| LastModified: Mar 21, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
