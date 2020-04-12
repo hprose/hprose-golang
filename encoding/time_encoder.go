18ae0cae8afd1b978b5b38f17a43ff07666b60fc
andot <mabingyao@gmail.com>
2020-04-12 13:58:48 +0800

    Change writeTime from func to method

--- encoding/time_encoder.go
+++ encoding/time_encoder.go
@@ -28,10 +28,10 @@ func (valenc timeEncoder) Encode(enc *Encoder, v interface{}) {
 
 func (timeEncoder) Write(enc *Encoder, v interface{}) {
 	enc.SetReference(v)
-	writeTime(enc, *(*time.Time)(reflect2.PtrOf(v)))
+	enc.writeTime(*(*time.Time)(reflect2.PtrOf(v)))
 }
 
-func writeDatePart(enc *Encoder, year int, month int, day int) {
+func (enc *Encoder) writeDatePart(year int, month int, day int) {
 	enc.buf = append(enc.buf, TagDate)
 	q := year / 100
 	p := q << 1
@@ -44,7 +44,7 @@ func writeDatePart(enc *Encoder, year int, month int, day int) {
 	enc.buf = append(enc.buf, digit2[p:p+2]...)
 }
 
-func writeTimePart(enc *Encoder, hour int, min int, sec int, nsec int) {
+func (enc *Encoder) writeTimePart(hour int, min int, sec int, nsec int) {
 	enc.buf = append(enc.buf, TagTime)
 	p := hour << 1
 	enc.buf = append(enc.buf, digit2[p:p+2]...)
@@ -75,17 +75,17 @@ func writeTimePart(enc *Encoder, hour int, min int, sec int, nsec int) {
 	return
 }
 
-func writeTime(enc *Encoder, t time.Time) {
+func (enc *Encoder) writeTime(t time.Time) {
 	year, month, day := t.Date()
 	hour, min, sec := t.Clock()
 	nsec := t.Nanosecond()
 	if (hour == 0) && (min == 0) && (sec == 0) && (nsec == 0) {
-		writeDatePart(enc, year, int(month), day)
+		enc.writeDatePart(year, int(month), day)
 	} else if (year == 1970) && (month == 1) && (day == 1) {
-		writeTimePart(enc, hour, min, sec, nsec)
+		enc.writeTimePart(hour, min, sec, nsec)
 	} else {
-		writeDatePart(enc, year, int(month), day)
-		writeTimePart(enc, hour, min, sec, nsec)
+		enc.writeDatePart(year, int(month), day)
+		enc.writeTimePart(hour, min, sec, nsec)
 	}
 	loc := TagSemicolon
 	if t.Location() == time.UTC {
