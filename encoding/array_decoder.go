cb1651ff807622624847d1b4c9b2af5b3f88913f
andot <mabingyao@gmail.com>
2020-06-13 18:21:43 +0800

    Imporved array decoder

--- encoding/array_decoder.go
+++ encoding/array_decoder.go
@@ -6,7 +6,7 @@
 |                                                          |
 | encoding/array_decoder.go                                |
 |                                                          |
-| LastModified: Jun 11, 2020                               |
+| LastModified: Jun 13, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -20,67 +20,178 @@ import (
 	"github.com/modern-go/reflect2"
 )
 
-func (dec *Decoder) readIntArray(p interface{}, et reflect.Type, length int) {
-	var slice []int
-	setSliceHeader(unsafe.Pointer(&slice), reflect2.PtrOf(p), length)
-	count := dec.ReadInt()
-	dec.AddReference(slice)
-	n := length
-	if n > count {
-		n = count
-	}
-	for i := 0; i < n; i++ {
-		slice[i] = dec.decodeInt(et, dec.NextByte())
-	}
-	switch {
-	case n < length:
-		for i := n; i < length; i++ {
-			slice[i] = 0
-		}
-	case n < count:
-		for i := n; i < count; i++ {
-			dec.decodeInt(et, dec.NextByte())
-		}
-	}
-	dec.Skip()
-}
-
 // arrayDecoder is the implementation of ValueDecoder for [N]T.
 type arrayDecoder struct {
-	t         *reflect2.UnsafeArrayType
-	et        reflect.Type
+	at        *reflect2.UnsafeArrayType
 	empty     unsafe.Pointer
-	readArray func(dec *Decoder, p interface{}, et reflect.Type, length int)
+	st        *reflect2.UnsafeSliceType
+	emptyElem unsafe.Pointer
+	tempElem  unsafe.Pointer
+	readElem  func(dec *Decoder, ep unsafe.Pointer)
 }
 
 func (valdec arrayDecoder) Decode(dec *Decoder, p interface{}, tag byte) {
 	switch tag {
 	case TagNull, TagEmpty:
-		valdec.t.UnsafeSet(reflect2.PtrOf(p), valdec.empty)
+		valdec.at.UnsafeSet(reflect2.PtrOf(p), valdec.empty)
 	case TagList:
-		valdec.readArray(dec, p, valdec.et, valdec.t.Len())
+		length := valdec.at.Len()
+		count := dec.ReadInt()
+		slice := reflect2.PtrOf(sliceHeader{reflect2.PtrOf(p), length, length})
+		dec.AddReference(p)
+		n := length
+		if n > count {
+			n = count
+		}
+		for i := 0; i < n; i++ {
+			valdec.readElem(dec, valdec.st.UnsafeGetIndex(slice, i))
+		}
+		switch {
+		case n < length:
+			for i := n; i < length; i++ {
+				valdec.st.UnsafeSetIndex(slice, i, valdec.emptyElem)
+			}
+		case n < count:
+			for i := n; i < count; i++ {
+				valdec.readElem(dec, valdec.tempElem)
+			}
+		}
+		dec.Skip()
 	default:
-		dec.decodeError(valdec.t.Type1(), tag)
+		dec.decodeError(valdec.at.Type1(), tag)
 	}
 }
 
 func (valdec arrayDecoder) Type() reflect.Type {
-	return valdec.t.Type1()
+	return valdec.at.Type1()
 }
 
 // ArrayDecoder returns a ValueDecoder for [N]T.
-func ArrayDecoder(t reflect.Type, readArray func(dec *Decoder, p interface{}, et reflect.Type, length int)) ValueDecoder {
-	t2 := reflect2.Type2(t).(*reflect2.UnsafeArrayType)
+func ArrayDecoder(t reflect.Type, readElem func(dec *Decoder, ep unsafe.Pointer)) ValueDecoder {
+	at := reflect2.Type2(t).(*reflect2.UnsafeArrayType)
+	et := t.Elem()
 	return arrayDecoder{
-		t2,
-		t.Elem(),
-		t2.UnsafeNew(),
-		readArray,
+		at,
+		at.UnsafeNew(),
+		reflect2.Type2(reflect.SliceOf(et)).(*reflect2.UnsafeSliceType),
+		reflect2.Type2(et).UnsafeNew(),
+		reflect2.Type2(et).UnsafeNew(),
+		readElem,
 	}
 }
 
+func boolArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		bdec.decode(dec, (*bool)(ep), dec.NextByte())
+	})
+}
+
 func intArrayDecoder(t reflect.Type) ValueDecoder {
-	return ArrayDecoder(t, func(dec *Decoder, p interface{}, et reflect.Type, length int) {
-		dec.readIntArray(p, et, length)
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		idec.decode(dec, (*int)(ep), dec.NextByte())
+	})
+}
+
+func int8ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		i8dec.decode(dec, (*int8)(ep), dec.NextByte())
+	})
+}
+
+func int16ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		i16dec.decode(dec, (*int16)(ep), dec.NextByte())
+	})
+}
+
+func int32ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		i32dec.decode(dec, (*int32)(ep), dec.NextByte())
+	})
+}
+
+func int64ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		i64dec.decode(dec, (*int64)(ep), dec.NextByte())
+	})
+}
+
+func uintArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		udec.decode(dec, (*uint)(ep), dec.NextByte())
+	})
+}
+
+func uint8ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		u8dec.decode(dec, (*uint8)(ep), dec.NextByte())
+	})
+}
+
+func uint16ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		u16dec.decode(dec, (*uint16)(ep), dec.NextByte())
+	})
+}
+
+func uint32ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		u32dec.decode(dec, (*uint32)(ep), dec.NextByte())
+	})
+}
+
+func uint64ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		u64dec.decode(dec, (*uint64)(ep), dec.NextByte())
+	})
+}
+
+func uintptrArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		updec.decode(dec, (*uintptr)(ep), dec.NextByte())
+	})
+}
+
+func float32ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		f32dec.decode(dec, (*float32)(ep), dec.NextByte())
+	})
+}
+
+func float64ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		f64dec.decode(dec, (*float64)(ep), dec.NextByte())
+	})
+}
+
+func complex64ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		c64dec.decode(dec, (*complex64)(ep), dec.NextByte())
+	})
+}
+
+func complex128ArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		c128dec.decode(dec, (*complex128)(ep), dec.NextByte())
+	})
+}
+
+func interfaceArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		ifdec.decode(dec, (*interface{})(ep), dec.NextByte())
+	})
+}
+
+func stringArrayDecoder(t reflect.Type) ValueDecoder {
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		sdec.decode(dec, (*string)(ep), dec.NextByte())
+	})
+}
+
+func otherArrayDecoder(t reflect.Type) ValueDecoder {
+	valdec := getValueDecoder(t.Elem())
+	et := reflect2.Type2(t.Elem())
+	return ArrayDecoder(t, func(dec *Decoder, ep unsafe.Pointer) {
+		valdec.Decode(dec, et.UnsafeIndirect(ep), dec.NextByte())
 	})
 }
--- encoding/array_decoder_test.go
+++ encoding/array_decoder_test.go
@@ -6,7 +6,7 @@
 |                                                          |
 | encoding/array_decoder_test.go                           |
 |                                                          |
-| LastModified: Jun 11, 2020                               |
+| LastModified: Jun 13, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -73,6 +73,56 @@ func TestDecodeCustomIntArray(t *testing.T) {
 	assert.EqualError(t, dec.Error, `hprose/encoding: can not cast int to [5]encoding.Int`) // 1
 }
 
+func TestDecodeInterfaceArray(t *testing.T) {
+	sb := new(strings.Builder)
+	enc := NewEncoder(sb)
+	enc.Encode([]int{1, 2, 3, 4, 5, 6})
+	enc.Encode([]float32{4, 3, 2, 1})
+	enc.Encode([]string{"1", "2", "3", "4", "5"})
+	enc.Encode(nil)
+	enc.Encode("")
+	enc.Encode(1)
+	dec := NewDecoder(([]byte)(sb.String()))
+	var array [5]interface{}
+	dec.Decode(&array)
+	assert.Equal(t, [...]interface{}{1, 2, 3, 4, 5}, array) // []int{1, 2, 3, 4, 5, 6}
+	dec.Decode(&array)
+	assert.Equal(t, [...]interface{}{4.0, 3.0, 2.0, 1.0, nil}, array) // []float32{4, 3, 2, 1}
+	dec.Decode(&array)
+	assert.Equal(t, [...]interface{}{"1", "2", "3", "4", "5"}, array) // []string{"1", "2", "3", "4", "5"}
+	dec.Decode(&array)
+	assert.Equal(t, [5]interface{}{}, array) // nil
+	dec.Decode(&array)
+	assert.Equal(t, [5]interface{}{}, array) // ""
+	dec.Decode(&array)
+	assert.EqualError(t, dec.Error, `hprose/encoding: can not cast int to [5]interface {}`) // 1
+}
+
+func TestDecodeIntIntArray(t *testing.T) {
+	sb := new(strings.Builder)
+	enc := NewEncoder(sb)
+	enc.Encode([][]int{{1, 2, 3}, {4, 5, 6}})
+	enc.Encode([][]float32{{4, 3, 2}, {1}})
+	enc.Encode([][]string{{"1", "2", "3"}, {"4", "5"}})
+	enc.Encode(nil)
+	enc.Encode("")
+	enc.Encode(1)
+	dec := NewDecoder(([]byte)(sb.String()))
+	var array [2][3]int
+	dec.Decode(&array)
+	assert.Equal(t, [2][3]int{{1, 2, 3}, {4, 5, 6}}, array) // [][]int{{1, 2, 3}, {4, 5, 6}}
+	dec.Decode(&array)
+	assert.Equal(t, [2][3]int{{4, 3, 2}, {1, 0, 0}}, array) // [][]float32{{4, 3, 2}, {1}}
+	dec.Decode(&array)
+	assert.Equal(t, [2][3]int{{1, 2, 3}, {4, 5, 0}}, array) // [][]string{{"1", "2", "3"}, {"4", "5"}}
+	dec.Decode(&array)
+	assert.Equal(t, [2][3]int{}, array) // nil
+	dec.Decode(&array)
+	assert.Equal(t, [2][3]int{}, array) // ""
+	dec.Decode(&array)
+	assert.EqualError(t, dec.Error, `hprose/encoding: can not cast int to [2][3]int`) // 1
+}
+
 func BenchmarkDecodeIntArray(b *testing.B) {
 	sb := new(strings.Builder)
 	enc := NewEncoder(sb)
@@ -104,6 +154,88 @@ func BenchmarkJsonDecodeIntArray(b *testing.B) {
 	enc.Encode("")
 	data := ([]byte)(sb.String())
 	var array [5]int
+	for i := 0; i < b.N; i++ {
+		dec := jsoniter.NewDecoder(bytes.NewReader(data))
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+	}
+}
+
+func BenchmarkDecodeInterfaceArray(b *testing.B) {
+	sb := new(strings.Builder)
+	enc := NewEncoder(sb)
+	enc.Encode([]int{1, 2, 3, 4, 5})
+	enc.Encode([]float32{1, 2, 3, 4, 5})
+	enc.Encode([]string{"1", "2", "3", "4", "5"})
+	enc.Encode(nil)
+	enc.Encode("")
+	data := ([]byte)(sb.String())
+	dec := &Decoder{}
+	var array [5]interface{}
+	for i := 0; i < b.N; i++ {
+		dec.ResetBytes(data)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+	}
+}
+
+func BenchmarkJsonDecodeInterfaceArray(b *testing.B) {
+	sb := new(strings.Builder)
+	enc := jsoniter.NewEncoder(sb)
+	enc.Encode([]int{1, 2, 3, 4, 5})
+	enc.Encode([]float32{1, 2, 3, 4, 5})
+	enc.Encode([]string{"1", "2", "3", "4", "5"})
+	enc.Encode(nil)
+	enc.Encode("")
+	data := ([]byte)(sb.String())
+	var array [5]interface{}
+	for i := 0; i < b.N; i++ {
+		dec := jsoniter.NewDecoder(bytes.NewReader(data))
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+	}
+}
+
+func BenchmarkDecodeIntIntArray(b *testing.B) {
+	sb := new(strings.Builder)
+	enc := NewEncoder(sb)
+	enc.Encode([][]int{{1, 2, 3}, {4, 5, 6}})
+	enc.Encode([][]float32{{4, 3, 2}, {1}})
+	enc.Encode([][]string{{"1", "2", "3"}, {"4", "5"}})
+	enc.Encode(nil)
+	enc.Encode("")
+	data := ([]byte)(sb.String())
+	dec := &Decoder{}
+	var array [2][3]int
+	for i := 0; i < b.N; i++ {
+		dec.ResetBytes(data)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+		dec.Decode(&array)
+	}
+}
+
+func BenchmarkJsonDecodeIntIntArray(b *testing.B) {
+	sb := new(strings.Builder)
+	enc := jsoniter.NewEncoder(sb)
+	enc.Encode([][]int{{1, 2, 3}, {4, 5, 6}})
+	enc.Encode([][]float32{{4, 3, 2}, {1}})
+	enc.Encode([][]string{{"1", "2", "3"}, {"4", "5"}})
+	enc.Encode(nil)
+	enc.Encode("")
+	data := ([]byte)(sb.String())
+	var array [2][3]int
 	for i := 0; i < b.N; i++ {
 		dec := jsoniter.NewDecoder(bytes.NewReader(data))
 		dec.Decode(&array)
