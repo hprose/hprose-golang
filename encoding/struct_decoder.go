126e81873ab4e66ffb112eb4dfcef0c4985770b9
andot <mabingyao@gmail.com>
2020-06-26 21:54:58 +0800

    Update struct_decoder.go

--- encoding/struct_decoder.go
+++ encoding/struct_decoder.go
@@ -21,6 +21,44 @@ import (
 	"github.com/modern-go/reflect2"
 )
 
+func (dec *Decoder) readObjectAsMap(structInfo structInfo) map[string]interface{} {
+	m := make(map[string]interface{}, len(structInfo.names))
+	t := reflect2.TypeOf(m).(*reflect2.UnsafeMapType)
+	dec.AddReference(m)
+	ptr := reflect2.PtrOf(&m)
+	for _, name := range structInfo.names {
+		v := dec.decodeInterface(interfaceType, dec.NextByte())
+		t.UnsafeSetIndex(ptr, reflect2.PtrOf(name), reflect2.PtrOf(&v))
+	}
+	dec.Skip()
+	return m
+}
+
+func (dec *Decoder) readObject(structInfo structInfo) interface{} {
+	obj := structInfo.t.New()
+	dec.AddReference(obj)
+	ptr := reflect2.PtrOf(obj)
+	for _, name := range structInfo.names {
+		if field, ok := structInfo.fields[name]; ok {
+			field.Decode(dec, field.Type.Type1(), field.Field.UnsafeGet(ptr))
+		} else {
+			_ = dec.decodeInterface(interfaceType, dec.NextByte())
+		}
+	}
+	dec.Skip()
+	return obj
+}
+
+// ReadObject reads object and add reference
+func (dec *Decoder) ReadObject() interface{} {
+	index := dec.ReadInt()
+	structInfo := dec.getStructInfo(index)
+	if structInfo.fields == nil {
+		return dec.readObjectAsMap(structInfo)
+	}
+	return dec.readObject(structInfo)
+}
+
 // structDecoder is the implementation of ValueEncoder for named struct.
 type structDecoder struct {
 	t      *reflect2.UnsafeStructType
