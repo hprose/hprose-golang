418e0143d64e7fa0c3035b608abe06c7dea0d398
andot <mabingyao@gmail.com>
2021-01-24 04:46:34 +0800

    Create service.go

---
+++ rpc/core/service.go
@@ -0,0 +1,18 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/service.go                                      |
+|                                                          |
+| LastModified: Jan 24, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
+
+// Service for RPC.
+type Service interface {
+}
