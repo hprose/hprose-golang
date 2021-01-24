178f0d37b5cd05c27c0b1beb7593f953f9139e97
andot <mabingyao@gmail.com>
2021-01-25 02:45:46 +0800

    Create plugin_manager.go

---
+++ rpc/core/plugin_manager.go
@@ -0,0 +1,91 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/plugin_manager.go                               |
+|                                                          |
+| LastModified: Jan 24, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
+
+import "sync"
+
+// NextPluginHandler must be one of NextInvokeHandler or NextIOHandler.
+type NextPluginHandler interface{}
+
+// PluginHandler must be one of InvokeHandler or IOHandler.
+type PluginHandler interface{}
+
+// PluginManager for RPC.
+type PluginManager interface {
+	Handler() NextPluginHandler
+	Use(handler ...PluginHandler)
+	Unuse(handler ...PluginHandler)
+}
+
+type pluginManager struct {
+	handlers       []PluginHandler
+	rwlock         *sync.RWMutex
+	defaultHandler NextPluginHandler
+	handler        NextPluginHandler
+	getNextHandler func(handler PluginHandler, next NextPluginHandler) NextPluginHandler
+}
+
+func newPluginManager(handler NextPluginHandler, getNextHandler func(handler PluginHandler, next NextPluginHandler) NextPluginHandler) PluginManager {
+	return &pluginManager{
+		rwlock:         &sync.RWMutex{},
+		handler:        handler,
+		defaultHandler: handler,
+		getNextHandler: getNextHandler,
+	}
+}
+
+func (pm *pluginManager) rebuildHandler() {
+	next := pm.defaultHandler
+	pm.rwlock.RLock()
+	n := len(pm.handlers)
+	for i := n - 1; i >= 0; i-- {
+		next = pm.getNextHandler(pm.handlers[i], next)
+	}
+	pm.rwlock.RUnlock()
+	pm.handler = next
+}
+
+func (pm *pluginManager) Handler() NextPluginHandler {
+	return pm.handler
+}
+
+func (pm *pluginManager) Use(handler ...PluginHandler) {
+	pm.rwlock.Lock()
+	pm.handlers = append(pm.handlers, handler...)
+	pm.rwlock.Unlock()
+	pm.rebuildHandler()
+}
+
+func (pm *pluginManager) Unuse(handler ...PluginHandler) {
+	rebuild := false
+	pm.rwlock.Lock()
+	var handlers []PluginHandler
+	for _, h := range pm.handlers {
+		for _, h2 := range handler {
+			if h == h2 {
+				h = nil
+				rebuild = true
+				break
+			}
+		}
+		if h != nil {
+			handlers = append(handlers, h)
+		}
+	}
+	pm.handlers = handlers
+	pm.rwlock.Unlock()
+	if rebuild {
+		pm.rebuildHandler()
+	}
+}
