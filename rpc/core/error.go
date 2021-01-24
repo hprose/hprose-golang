082dc5b3bf1676562f066ac026ab727c7dc6b46d
andot <mabingyao@gmail.com>
2021-01-25 02:45:38 +0800

    Create error.go

---
+++ rpc/core/error.go
@@ -0,0 +1,73 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/error.go                                        |
+|                                                          |
+| LastModified: Jan 24, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
+
+// ErrTimeout represents a timeout error
+var ErrTimeout = errors.New("timeout")
+
+// ErrServerIsAlreadyStarted represents a error
+var ErrServerIsAlreadyStarted = errors.New("The server is already started")
+
+// ErrServerIsNotStarted represents a error
+var ErrServerIsNotStarted = errors.New("The server is not started")
+
+// ErrClientIsAlreadyClosed represents a error
+var ErrClientIsAlreadyClosed = errors.New("The Client is already closed")
+
+// ErrURIListEmpty represents a error
+var ErrURIListEmpty = errors.New("uriList must contain at least one uri")
+
+// ErrNotSupportMultpleProtocol represents a error
+var ErrNotSupportMultpleProtocol = errors.New("Not support multiple protocol")
+
+// PanicError represents a panic error
+type PanicError struct {
+	Panic interface{}
+	Stack []byte
+}
+
+func stack() []byte {
+	buf := make([]byte, 1024)
+	for {
+		n := runtime.Stack(buf, false)
+		if n < len(buf) {
+			return buf[:n]
+		}
+		buf = make([]byte, 2*len(buf))
+	}
+}
+
+// NewPanicError return a panic error
+func NewPanicError(v interface{}) *PanicError {
+	return &PanicError{v, stack()}
+}
+
+// Error implements the PanicError Error method.
+func (pe *PanicError) Error() string {
+	return fmt.Sprintf("%v", pe.Panic)
+}
+
+// String returns the panic error message and stack.
+func (pe *PanicError) String() string {
+	return fmt.Sprintf("%v\r\n%s", pe.Panic, pe.Stack)
+}
