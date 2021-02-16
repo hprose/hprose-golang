bd78d8b7f21e243f688b938ca4a036ef6c499f46
小马哥 <mabingyao@gmail.com>
2021-02-17 01:26:34 +0800

    Update method_manager.go

--- rpc/core/method_manager.go
+++ rpc/core/method_manager.go
@@ -23,6 +23,7 @@ import (
 type MethodManager interface {
 	Get(name string) Method
 	Names() (names []string)
+	Methods() (methods []Method)
 	Remove(name string)
 	Add(method Method)
 	AddFunction(f interface{}, name string)
@@ -35,22 +36,25 @@ type MethodManager interface {
 }
 
 type methodManager struct {
-	methods *sync.Map
+	methods sync.Map
 }
 
 // NewMethodManager returns a MethodManager
 func NewMethodManager() MethodManager {
-	return methodManager{&sync.Map{}}
+	return &methodManager{}
 }
 
-func (mm methodManager) Get(name string) Method {
+func (mm *methodManager) Get(name string) Method {
 	if method, ok := mm.methods.Load(strings.ToLower(name)); ok {
+		return method.(Method)
+	}
+	if method, ok := mm.methods.Load("*"); ok {
 		return method.(Method)
 	}
 	return nil
 }
 
-func (mm methodManager) Names() (names []string) {
+func (mm *methodManager) Names() (names []string) {
 	mm.methods.Range(func(key, value interface{}) bool {
 		names = append(names, value.(Method).Name())
 		return true
@@ -58,7 +62,7 @@ func (mm methodManager) Names() (names []string) {
 	return
 }
 
-func (mm methodManager) Methods() (methods []Method) {
+func (mm *methodManager) Methods() (methods []Method) {
 	mm.methods.Range(func(key, value interface{}) bool {
 		methods = append(methods, value.(Method))
 		return true
@@ -66,15 +70,15 @@ func (mm methodManager) Methods() (methods []Method) {
 	return
 }
 
-func (mm methodManager) Remove(name string) {
+func (mm *methodManager) Remove(name string) {
 	mm.methods.Delete(strings.ToLower(name))
 }
 
-func (mm methodManager) Add(method Method) {
+func (mm *methodManager) Add(method Method) {
 	mm.methods.Store(strings.ToLower(method.Name()), method)
 }
 
-func (mm methodManager) AddFunction(f interface{}, name string) {
+func (mm *methodManager) AddFunction(f interface{}, name string) {
 	var method Method
 	switch f := f.(type) {
 	case reflect.Value:
@@ -97,14 +101,14 @@ func (mm methodManager) AddFunction(f interface{}, name string) {
 	mm.Add(method)
 }
 
-func (mm methodManager) addFunction(f interface{}, name string, namespace ...string) {
+func (mm *methodManager) addFunction(f interface{}, name string, namespace ...string) {
 	if len(namespace) > 0 && namespace[0] != "" {
 		name = namespace[0] + "_" + name
 	}
 	mm.AddFunction(f, name)
 }
 
-func (mm methodManager) AddMethod(name string, target interface{}, alias ...string) {
+func (mm *methodManager) AddMethod(name string, target interface{}, alias ...string) {
 	obj := reflect.ValueOf(target)
 	f := obj.MethodByName(name)
 	if f.Kind() != reflect.Func && obj.Kind() == reflect.Struct {
@@ -125,7 +129,7 @@ func (mm methodManager) AddMethod(name string, target interface{}, alias ...stri
 	}
 }
 
-func (mm methodManager) addMethod(name string, target interface{}, namespace ...string) {
+func (mm *methodManager) addMethod(name string, target interface{}, namespace ...string) {
 	alias := ""
 	if len(namespace) > 0 && namespace[0] != "" {
 		alias = namespace[0] + "_" + name
@@ -133,13 +137,13 @@ func (mm methodManager) addMethod(name string, target interface{}, namespace ...
 	mm.AddMethod(name, target, alias)
 }
 
-func (mm methodManager) AddMethods(names []string, target interface{}, namespace ...string) {
+func (mm *methodManager) AddMethods(names []string, target interface{}, namespace ...string) {
 	for _, name := range names {
 		mm.addMethod(name, target, namespace...)
 	}
 }
 
-func (mm methodManager) addMethods(v reflect.Value, t reflect.Type, namespace ...string) {
+func (mm *methodManager) addMethods(v reflect.Value, t reflect.Type, namespace ...string) {
 	n := t.NumMethod()
 	for i := 0; i < n; i++ {
 		name := t.Method(i).Name
@@ -158,7 +162,7 @@ func getPtrTo(v reflect.Value, t reflect.Type, kind reflect.Kind) (reflect.Value
 	return v, t
 }
 
-func (mm methodManager) addFuncField(v reflect.Value, t reflect.Type, i int, namespace ...string) {
+func (mm *methodManager) addFuncField(v reflect.Value, t reflect.Type, i int, namespace ...string) {
 	f := v.Field(i)
 	name := t.Field(i).Name
 	if f.IsValid() {
@@ -169,7 +173,7 @@ func (mm methodManager) addFuncField(v reflect.Value, t reflect.Type, i int, nam
 	}
 }
 
-func (mm methodManager) recursiveAddFuncFields(v reflect.Value, t reflect.Type, i int, namespace ...string) {
+func (mm *methodManager) recursiveAddFuncFields(v reflect.Value, t reflect.Type, i int, namespace ...string) {
 	f := v.Field(i)
 	fs := t.Field(i)
 	name := fs.Name
@@ -206,7 +210,7 @@ func (mm methodManager) recursiveAddFuncFields(v reflect.Value, t reflect.Type, 
 
 type addFuncFunc func(v reflect.Value, t reflect.Type, i int, namespace ...string)
 
-func (mm methodManager) addInstanceMethods(target interface{}, addFunc addFuncFunc, namespace ...string) {
+func (mm *methodManager) addInstanceMethods(target interface{}, addFunc addFuncFunc, namespace ...string) {
 	if target == nil {
 		panic("target can't be nil")
 	}
@@ -223,7 +227,7 @@ func (mm methodManager) addInstanceMethods(target interface{}, addFunc addFuncFu
 }
 
 // AddInstanceMethods is used for publishing all the public methods and func fields with namespace.
-func (mm methodManager) AddInstanceMethods(target interface{}, namespace ...string) {
+func (mm *methodManager) AddInstanceMethods(target interface{}, namespace ...string) {
 	mm.addInstanceMethods(target, mm.addFuncField, namespace...)
 }
 
@@ -231,18 +235,18 @@ func (mm methodManager) AddInstanceMethods(target interface{}, namespace ...stri
 // obj self and on its anonymous or non-anonymous struct fields (or pointer to
 // pointer ... to pointer struct fields). This is a recursive operation.
 // So it's a pit, if you do not know what you are doing, do not step on.
-func (mm methodManager) AddAllMethods(target interface{}, namespace ...string) {
+func (mm *methodManager) AddAllMethods(target interface{}, namespace ...string) {
 	mm.addInstanceMethods(target, mm.recursiveAddFuncFields, namespace...)
 }
 
 // AddMissingMethod is used for publishing a method,
 // all methods not explicitly published will be redirected to this method.
-func (mm methodManager) AddMissingMethod(f MissingMethod) {
+func (mm *methodManager) AddMissingMethod(f MissingMethod) {
 	mm.Add(NewMissingMethod(f))
 }
 
 // AddNetRPCMethods is used for publishing methods defined for net/rpc.
-func (mm methodManager) AddNetRPCMethods(rcvr interface{}, namespace ...string) {
+func (mm *methodManager) AddNetRPCMethods(rcvr interface{}, namespace ...string) {
 	if rcvr == nil {
 		panic("rcvr can't be nil")
 	}
@@ -260,7 +264,7 @@ func (mm methodManager) AddNetRPCMethods(rcvr interface{}, namespace ...string) 
 	}
 }
 
-func (mm methodManager) addNetRPCMethod(name string, method reflect.Value) {
+func (mm *methodManager) addNetRPCMethod(name string, method reflect.Value) {
 	ft := method.Type()
 	if ft.NumIn() != 2 || ft.IsVariadic() {
 		return
