29376639e222378afbc1487e262bd8d6c66063ea
小马哥 <mabingyao@gmail.com>
2021-02-17 06:41:06 +0800

    Create service_codec_test.go

---
+++ rpc/core/service_codec_test.go
@@ -0,0 +1,14 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/service_codec_test.go                           |
+|                                                          |
+| LastModified: Feb 17, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
