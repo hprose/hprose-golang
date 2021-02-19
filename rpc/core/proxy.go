2cee59718ea058a3f83e9f92292d37eb5c8d2b80
小马哥 <mabingyao@gmail.com>
2021-02-20 03:14:24 +0800

    Add proxy.

---
+++ rpc/core/proxy.go
@@ -0,0 +1,120 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/proxy.go                                        |
+|                                                          |
+| LastModified: Feb 20, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
+
+import (
+	"reflect"
+)
+
+// InvocationHandler for the proxy instance.
+type InvocationHandler func(proxy interface{}, method reflect.StructField, namespace string, args []interface{}) (results []interface{}, err error)
+
+// ProxyBuilder .
+type ProxyBuilder interface {
+	Build(proxy interface{}, handler InvocationHandler, namespace string)
+}
+
+// Proxy is a global ProxyBuilder.
+var Proxy ProxyBuilder = proxyBuilder{}
+
+type proxyBuilder struct{}
+
+func (b proxyBuilder) Build(proxy interface{}, handler InvocationHandler, namespace string) {
+	b.build(proxy, handler, namespace, reflect.ValueOf(proxy).Elem())
+}
+
+func (b proxyBuilder) build(proxy interface{}, handler InvocationHandler, namespace string, p reflect.Value) {
+	t := p.Type()
+	for t.Kind() == reflect.Ptr {
+		t = t.Elem()
+		if p.IsNil() {
+			setAccessible(p).Set(reflect.New(t))
+		}
+		p = p.Elem()
+	}
+	if t.Kind() != reflect.Struct {
+		return
+	}
+	n := p.NumField()
+	for i := 0; i < n; i++ {
+		f := p.Field(i)
+		ft := f.Type()
+		sf := t.Field(i)
+		switch ft.Kind() {
+		case reflect.Struct, reflect.Ptr:
+			ns := namespace
+			if !sf.Anonymous {
+				ns = sf.Name
+				if namespace != "" {
+					ns = namespace + "." + sf.Name
+				}
+			}
+			b.build(proxy, handler, ns, f)
+		case reflect.Func:
+			setAccessible(f).Set(b.method(proxy, handler, namespace, ft, sf))
+		}
+	}
+}
+
+func (b proxyBuilder) in(ft reflect.Type, in []reflect.Value) (args []interface{}) {
+	n := len(in)
+	m := n - 1
+	if ft.IsVariadic() {
+		n--
+		n += in[n].Len()
+	}
+	args = make([]interface{}, n)
+	if ft.IsVariadic() {
+		for i := 0; i < m; i++ {
+			args[i] = in[i].Interface()
+		}
+		v := in[m]
+		for i := 0; i < v.Len(); i++ {
+			args[m+i] = v.Index(i).Interface()
+		}
+	} else {
+		for i := 0; i < n; i++ {
+			args[i] = in[i].Interface()
+		}
+	}
+	return
+}
+
+func (b proxyBuilder) out(ft reflect.Type, results []interface{}, err error) (out []reflect.Value) {
+	n := ft.NumOut()
+	out = make([]reflect.Value, n)
+	if ft.Out(n-1) == errorType {
+		n--
+		out[n] = reflect.ValueOf(&err).Elem()
+	}
+	m := len(results)
+	if m > n {
+		m = n
+	}
+	for i := 0; i < m; i++ {
+		out[i] = reflect.ValueOf(results[i])
+	}
+	for i := m; i < n; i++ {
+		out[i] = reflect.Zero(ft.Out(i))
+	}
+	return
+}
+
+func (b proxyBuilder) method(proxy interface{}, h InvocationHandler, namespace string, ft reflect.Type, sf reflect.StructField) reflect.Value {
+	return reflect.MakeFunc(ft, func(in []reflect.Value) (out []reflect.Value) {
+		args := b.in(ft, in)
+		results, err := h(proxy, sf, namespace, args)
+		return b.out(ft, results, err)
+	})
+}
---
+++ rpc/core/proxy_test.go
@@ -0,0 +1,84 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/core/proxy_test.go                                   |
+|                                                          |
+| LastModified: Feb 20, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package core
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProxy(t *testing.T) {
+	type testEmbedStruct struct {
+		Test func() string
+	}
+	type testStruct struct {
+		Test  func() string
+		Test2 func(a, b int) (int, int)
+		Test3 func(operate string, x ...int) (int, error)
+		Embed struct {
+			Test  func() string
+			embed ***testEmbedStruct
+		}
+		*testEmbedStruct
+		*int
+		b bool
+	}
+	testInvocationHandler := func(proxy interface{}, method reflect.StructField, namespace string, args []interface{}) (results []interface{}, err error) {
+		switch method.Name {
+		case "Test":
+			return []interface{}{"Hello World!"}, nil
+		case "Test2":
+			return []interface{}{args[1], args[0]}, nil
+		case "Test3":
+			n := 0
+			for i := 1; i < len(args); i++ {
+				switch args[0] {
+				case "sum":
+					n += args[i].(int)
+				case "mul":
+					if i == 1 {
+						n = 1
+					}
+					n *= args[i].(int)
+				default:
+					return nil, errors.New("unknown operate")
+				}
+			}
+			return []interface{}{n}, nil
+		}
+		return nil, nil
+	}
+	var testp **testStruct
+	Proxy.Build(&testp, testInvocationHandler, "")
+	test := *testp
+	assert.Equal(t, `Hello World!`, test.Test())
+	a, b := test.Test2(123, 456)
+	assert.Equal(t, a, 456)
+	assert.Equal(t, b, 123)
+	n, err := test.Test3("sum", 1, 2, 3, 4)
+	assert.Equal(t, 10, n)
+	assert.NoError(t, err)
+	n, err = test.Test3("mul", 1, 2, 3, 4)
+	assert.Equal(t, 24, n)
+	assert.NoError(t, err)
+	n, err = test.Test3("div", 1, 2, 3, 4)
+	assert.Equal(t, 0, n)
+	assert.Error(t, err)
+	assert.Equal(t, `Hello World!`, test.Embed.Test())
+	assert.Equal(t, `Hello World!`, (**test.Embed.embed).Test())
+	assert.Equal(t, `Hello World!`, test.testEmbedStruct.Test())
+}
