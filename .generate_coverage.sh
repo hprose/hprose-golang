6627ca19cae31a72068120dde3532e2cfdf56a5e
小马哥 <mabingyao@gmail.com>
2021-02-22 00:55:22 +0800

    Update .travis.yml

--- .generate_coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-# Requires installation of: `github.com/wadey/gocovmerge`
-
-cd $GOPATH/src/github.com/hprose/hprose-golang
-go test -v -covermode=atomic -coverprofile=full_cov.out -coverpkg=./... ./...
