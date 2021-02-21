26dc89a9104c72cec5a43423b52a049183c269b7
小马哥 <mabingyao@gmail.com>
2021-02-21 18:55:54 +0800

    Fixed timeout plugin.

--- rpc/plugins/timeout/timeout.go
+++ rpc/plugins/timeout/timeout.go
@@ -28,20 +28,19 @@ type ExecuteTimeout struct {
 // Handler for ExecuteTimeout.
 func (et ExecuteTimeout) Handler(ctx context.Context, name string, args []interface{}, next core.NextInvokeHandler) (result []interface{}, err error) {
 	timeout := et.Timeout
-	if c, ok := core.FromContext(ctx); ok {
-		if t, ok := c.Items().Get("timeout"); ok {
-			switch t := t.(type) {
-			case time.Duration:
-				timeout = t
-			case int:
-				timeout = time.Duration(t)
-			case uint:
-				timeout = time.Duration(t)
-			case int64:
-				timeout = time.Duration(t)
-			case uint64:
-				timeout = time.Duration(t)
-			}
+	serviceContext := core.GetServiceContext(ctx)
+	if t, ok := serviceContext.Method.Options().Get("timeout"); ok {
+		switch t := t.(type) {
+		case time.Duration:
+			timeout = t
+		case int:
+			timeout = time.Duration(t)
+		case uint:
+			timeout = time.Duration(t)
+		case int64:
+			timeout = time.Duration(t)
+		case uint64:
+			timeout = time.Duration(t)
 		}
 	}
 	if timeout <= 0 {
@@ -65,6 +64,9 @@ func (et ExecuteTimeout) Handler(ctx context.Context, name string, args []interf
 }
 
 // GetHandler returns an execute timeout invoke handler.
-func GetHandler(timeout time.Duration) core.InvokeHandler {
-	return ExecuteTimeout{timeout}.Handler
+func GetHandler(timeout ...time.Duration) core.InvokeHandler {
+	if len(timeout) > 0 {
+		return ExecuteTimeout{timeout[0]}.Handler
+	}
+	return ExecuteTimeout{time.Second * 30}.Handler
 }
