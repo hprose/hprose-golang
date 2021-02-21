b0a2ab6cc86c4a102b1757a7bd783426941129e9
小马哥 <mabingyao@gmail.com>
2021-02-21 18:50:52 +0800

    Add timeout plugin.

---
+++ rpc/plugins/timeout/timeout.go
@@ -0,0 +1,70 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| rpc/plugins/timeout/timeout.go                           |
+|                                                          |
+| LastModified: Feb 21, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package timeout
+
+import (
+	"context"
+	"time"
+
+	"github.com/hprose/hprose-golang/v3/rpc/core"
+)
+
+// ExecuteTimeout .
+type ExecuteTimeout struct {
+	Timeout time.Duration
+}
+
+// Handler for ExecuteTimeout.
+func (et ExecuteTimeout) Handler(ctx context.Context, name string, args []interface{}, next core.NextInvokeHandler) (result []interface{}, err error) {
+	timeout := et.Timeout
+	if c, ok := core.FromContext(ctx); ok {
+		if t, ok := c.Items().Get("timeout"); ok {
+			switch t := t.(type) {
+			case time.Duration:
+				timeout = t
+			case int:
+				timeout = time.Duration(t)
+			case uint:
+				timeout = time.Duration(t)
+			case int64:
+				timeout = time.Duration(t)
+			case uint64:
+				timeout = time.Duration(t)
+			}
+		}
+	}
+	if timeout <= 0 {
+		return next(ctx, name, args)
+	}
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(ctx, timeout)
+	defer cancel()
+	ch := make(chan struct{}, 1)
+	go func() {
+		result, err = next(ctx, name, args)
+		ch <- struct{}{}
+		close(ch)
+	}()
+	select {
+	case <-ctx.Done():
+		return nil, core.ErrTimeout
+	case <-ch:
+		return result, err
+	}
+}
+
+// GetHandler returns an execute timeout invoke handler.
+func GetHandler(timeout time.Duration) core.InvokeHandler {
+	return ExecuteTimeout{timeout}.Handler
+}
