8690d7d8a902ce26e3ef80e2b6e903e662f901f6
andot <mabingyao@gmail.com>
2021-05-17 00:08:52 +0800

    Add Marshal and Unmarshal

---
+++ encoding/formatter.go
@@ -0,0 +1,28 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| encoding/formatter.go                                    |
+|                                                          |
+| LastModified: May 16, 2021                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+package encoding
+
+func Marshal(v interface{}) ([]byte, error) {
+	encoder := new(Encoder)
+	if err := encoder.Encode(v); err != nil {
+		return nil, err
+	}
+	return encoder.Bytes(), nil
+}
+
+func Unmarshal(data []byte, v interface{}) error {
+	decoder := NewDecoder(data)
+	decoder.Decode(v)
+	return decoder.Error
+}
