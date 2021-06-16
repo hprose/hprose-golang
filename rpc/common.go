8a2ded7acc4ce73fc3e2152a1844d4fef968e3ba
小马哥 <mabingyao@gmail.com>
2021-06-16 15:57:39 +0800

    Update common.go

--- rpc/common.go
+++ rpc/common.go
@@ -6,7 +6,7 @@
 |                                                          |
 | rpc/common.go                                            |
 |                                                          |
-| LastModified: May 12, 2021                               |
+| LastModified: Jun 16, 2021                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -16,45 +16,83 @@ package rpc
 import "github.com/hprose/hprose-golang/v3/rpc/core"
 
 type (
-	Context                    = core.Context
-	ClientContext              = core.ClientContext
-	ServiceContext             = core.ServiceContext
-	InvalidRequestError        = core.InvalidRequestError
-	InvalidResponseError       = core.InvalidResponseError
-	PanicError                 = core.PanicError
-	UnsupportedProtocolError   = core.UnsupportedProtocolError
+	// Context for RPC.
+	Context = core.Context
+	// ClientContext for RPC.
+	ClientContext = core.ClientContext
+	// ServiceContext for RPC.
+	ServiceContext = core.ServiceContext
+	// InvalidRequestError represents a error.
+	InvalidRequestError = core.InvalidRequestError
+	// InvalidResponseError represents a error.
+	InvalidResponseError = core.InvalidResponseError
+	// PanicError represents a panic error.
+	PanicError = core.PanicError
+	// UnsupportedProtocolError represents a error.
+	UnsupportedProtocolError = core.UnsupportedProtocolError
+	// UnsupportedServerTypeError represents a error.
 	UnsupportedServerTypeError = core.UnsupportedServerTypeError
-	NextIOHandler              = core.NextIOHandler
-	NextInvokeHandler          = core.NextInvokeHandler
-	Method                     = core.Method
-	ClientCodec                = core.ClientCodec
-	ServiceCodec               = core.ServiceCodec
-	CodecOption                = core.CodecOption
+	// NextIOHandler for RPC.
+	NextIOHandler = core.NextIOHandler
+	// NextInvokeHandler for RPC.
+	NextInvokeHandler = core.NextInvokeHandler
+	// Method for RPC.
+	Method = core.Method
+	// ClientCodec for RPC.
+	ClientCodec = core.ClientCodec
+	// ServiceCodec for RPC.
+	ServiceCodec = core.ServiceCodec
+	// CodecOption for clientCodec & serviceCodec.
+	CodecOption = core.CodecOption
 )
 
 var (
-	ErrClosed                = core.ErrClosed
+	// ErrClosed represents a error.
+	ErrClosed = core.ErrClosed
+	// ErrRequestEntityTooLarge represents a error.
 	ErrRequestEntityTooLarge = core.ErrRequestEntityTooLarge
-	ErrTimeout               = core.ErrTimeout
-	IsTemporaryError         = core.IsTemporaryError
-	IsTimeoutError           = core.IsTimeoutError
-	RegisterHandler          = core.RegisterHandler
-	RegisterTransport        = core.RegisterTransport
-	WithContext              = core.WithContext
-	GetClientContext         = core.GetClientContext
-	NewClientContext         = core.NewClientContext
-	GetServiceContext        = core.GetServiceContext
-	NewServiceContext        = core.NewServiceContext
-	FromContext              = core.FromContext
-	NewContext               = core.NewContext
-	NewPanicError            = core.NewPanicError
-	MissingMethod            = core.MissingMethod
-	NewMethod                = core.NewMethod
-	NewClientCodec           = core.NewClientCodec
-	NewServiceCodec          = core.NewServiceCodec
-	WithDebug                = core.WithDebug
-	WithLongType             = core.WithLongType
-	WithMapType              = core.WithMapType
-	WithRealType             = core.WithRealType
-	WithSimple               = core.WithSimple
+	// ErrTimeout represents a error.
+	ErrTimeout = core.ErrTimeout
+	// IsTemporaryError returns true if err is a temporary error.
+	IsTemporaryError = core.IsTemporaryError
+	// IsTimeoutError returns true if err is a timeout error.
+	IsTimeoutError = core.IsTimeoutError
+	// RegisterHandler for Service.
+	RegisterHandler = core.RegisterHandler
+	// RegisterTransport for Client.
+	RegisterTransport = core.RegisterTransport
+	// WithContext returns a copy of the parent context and associates it with a rpc.Context.
+	WithContext = core.WithContext
+	// GetClientContext returns the *rpc.ClientContext bound to the context.
+	GetClientContext = core.GetClientContext
+	// NewClientContext returns a rpc.ClientContext.
+	NewClientContext = core.NewClientContext
+	// GetServiceContext returns the *rpc.ServiceContext bound to the context.
+	GetServiceContext = core.GetServiceContext
+	// NewServiceContext returns a rpc.ServiceContext.
+	NewServiceContext = core.NewServiceContext
+	// FromContext returns the rpc.Context bound to the context.
+	FromContext = core.FromContext
+	// NewContext returns a rpc.Context.
+	NewContext = core.NewContext
+	// NewPanicError return a panic error.
+	NewPanicError = core.NewPanicError
+	// MissingMethod returns a missing Method object.
+	MissingMethod = core.MissingMethod
+	// NewMethod returns a Method object.
+	NewMethod = core.NewMethod
+	// NewClientCodec returns the ClientCodec.
+	NewClientCodec = core.NewClientCodec
+	// NewServiceCodec returns the ServiceCodec.
+	NewServiceCodec = core.NewServiceCodec
+	// WithDebug returns a debug Option for clientCodec & serviceCodec.
+	WithDebug = core.WithDebug
+	// WithLongType returns a longType Option for clientCodec & serviceCodec.
+	WithLongType = core.WithLongType
+	// WithMapType returns a mapType Option for clientCodec & serviceCodec.
+	WithMapType = core.WithMapType
+	// WithRealType returns a realType Option for clientCodec & serviceCodec.
+	WithRealType = core.WithRealType
+	// WithSimple returns a simple Option for clientCodec & serviceCodec.
+	WithSimple = core.WithSimple
 )
