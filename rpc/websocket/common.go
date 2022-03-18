d29eadc41f5eb8db7292bfe0011b397a52af6411
小马哥 <mabingyao@gmail.com>
2022-03-18 21:39:09 +0800

    bounds-checking elimination

--- rpc/websocket/common.go
+++ rpc/websocket/common.go
@@ -6,7 +6,7 @@
 |                                                          |
 | rpc/websocket/common.go                                  |
 |                                                          |
-| LastModified: May 5, 2021                                |
+| LastModified: Mar 18, 2022                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -20,15 +20,15 @@ type data struct {
 }
 
 func makeHeader(index int) (header [4]byte) {
-	header[0] = byte(index >> 24 & 0xff)
-	header[1] = byte(index >> 16 & 0xff)
-	header[2] = byte(index >> 8 & 0xff)
 	header[3] = byte(index & 0xff)
+	header[2] = byte(index >> 8 & 0xff)
+	header[1] = byte(index >> 16 & 0xff)
+	header[0] = byte(index >> 24 & 0xff)
 	return
 }
 
 func parseHeader(header []byte) (index int, ok bool) {
-	index = int(header[0])<<24 | int(header[1])<<16 | int(header[2])<<8 | int(header[3])
+	index = int(header[3]) | int(header[2])<<8 | int(header[1])<<16 | int(header[0])<<24
 	if ok = (header[0]&0x80 == 0); !ok {
 		index &= 0x7fffffff
 	}
